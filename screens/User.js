d848a8d7e11294b19ec121146df65e16040e4a81
Harugatto <jihojiho2003@gmail.com>
2019-08-06 17:46:21 +0900

    fix: Mypage frontend edit finish

--- screens/User.js
+++ screens/User.js
@@ -18,6 +18,7 @@ import {
   Button
 } from 'react-native';
 import Navbar from '../components/Navbar';
+import moment from '../time.js';
 
 const win = Dimensions.get('window');
 
@@ -35,7 +36,7 @@ const styles = StyleSheet.create({
     paddingTop: 60,
     marginHorizontal: 40,
   },
-  image: {
+  profileimage: {
     height: 120,
     width: 120,
     alignSelf: 'center',
@@ -95,7 +96,70 @@ const styles = StyleSheet.create({
     fontFamily: 'NotoSansCJKkr-Regular',
     fontSize: 15,
     textAlign: 'center',
-  }
+  },
+  question: {
+    paddingTop: 12,
+    paddingBottom: 8,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignContent: 'space-between',
+    borderBottomColor: '#E2E2E2',
+    borderBottomWidth: 2,
+  },
+  questionLast: {
+    paddingTop: 12,
+    paddingBottom: 8,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    flexDirection: 'row',
+    alignContent: 'space-between',
+  },
+  eye: {
+    width: 18,
+    height: 9,
+  },
+  nontitle: {
+    fontFamily: 'NotoSansCJKkr-Regular',
+    fontSize: 15,
+    lineHeight: 15 * 1.4,
+    color: '#E2E2E2',
+    marginLeft: 5,
+  },
+  title: {
+    fontFamily: 'NotoSansCJKkr-Regular',
+    fontSize: 20,
+    lineHeight: 25 * 1.4,
+    color: 'black',
+  },
+  ongoing: {
+    fontFamily: 'NotoSansCJKkr-Regular',
+    fontSize: 15,
+    lineHeight: 15 * 1.4,
+    color: 'red',
+    marginLeft: 5,
+  },
+  complete: {
+    fontFamily: 'NotoSansCJKkr-Regular',
+    fontSize: 15,
+    lineHeight: 15 * 1.4,
+    color: highlightColor,
+    marginLeft: 5,
+  },
+  quesimage: {
+    height: 90,
+    width: 90,
+    alignSelf: 'center',
+    borderColor: '#E2E2E2',
+    borderWidth: 2,
+  },
+  ques: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingLeft: 3,
+  },
 })
 
 const exampleProfile = {
@@ -104,16 +168,20 @@ const exampleProfile = {
   school: '한국디지털미디어고등학교',
   question: [
     {
-      image: '../assets/examples/math.png',
+      image: require('../assets/examples/math.png'),
       title: '이 문제의 풀이과정을 잘 모르겠습니다.',
-      view: 30,
-      answer: 3,
+      views: 30,
+      answers: 3,
+      state: '진행중',
+      timestamp: 1564975538,
     },
     {
-      image: '../assets/examples/english.png',
+      image: require('../assets/examples/english.png'),
       title: '이게 무슨문제죠...?',
-      view: 10,
-      answer: 1,
+      views: 10,
+      answers: 1,
+      state: '답변 완료',
+      timestamp: 1564550000,
     },
   ],
 }
@@ -129,13 +197,14 @@ class MyPage extends Component {
 
   render(){
     const { navigation: { state: { routeName } } } = this.props;
+    const { questions } = this.state;
     return (
       <View style = {styles.container}>
         <View style = {styles.profile}>
           <Image
             source={require('../assets/examples/profile.png')}
             resizeMode="cover"
-            style={styles.image}
+            style={styles.profileimage}
           />
           <View style = {styles.spec}>
             <View style = {styles.spec1}>
@@ -161,7 +230,50 @@ class MyPage extends Component {
         <ScrollView>
           <View>
             {questions.map((item, idx) => (
-              
+              <TouchableOpacity 
+                key={idx}
+              >
+                <View
+                  style={[(idx === questions.length - 1) ? styles.questionLast: styles.question ]}
+                >
+                  <View style={{flex: 3, justifyContent: 'center', margin: 5}}>
+                    <View style={styles.ques}>
+                      <Text style={{
+                        fontFamily: 'NotoSansCJKkr-Regular',
+                        fontSize: 15,
+                        lineHeight: 15 * 1.4,
+                        color: '#E2E2E2',
+                      }}>
+                        {moment.unix(item.timestamp).format('YY.DD.MM')}
+                      </Text>
+                      <Text style={(item.state === '진행중') ? styles.ongoing : styles.complete}>
+                        {item.state}
+                      </Text>
+                    </View>
+                    <Text style={styles.title}>
+                      {item.title}
+                    </Text>
+                    <View style={styles.ques}>
+                      <Image
+                        source={require('../assets/icons/eye.png')}
+                        style={styles.eye}
+                      /> 
+                      <Text style={styles.nontitle}>
+                        {item.views} 
+                      </Text>
+                      <Text style={styles.nontitle}>
+                        답변 {item.answers}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{flex: 1, justifyContent: 'center', margin: 5}}>
+                    <Image
+                      source={item.image}
+                      style={styles.quesimage}
+                    />
+                  </View>
+                </View>
+              </TouchableOpacity>
             ))}
           </View>
         </ScrollView>
